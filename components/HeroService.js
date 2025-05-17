f8e1c31f75847592bd47d6ed187d6c1f38268751
unknown <nguruedwin46@gmail.com>
2025-05-18 00:48:13 +0300

    changed layout

--- components/HeroService.js
+++ components/HeroService.js
@@ -44,7 +44,7 @@ export default function ServicesHero() {
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
-          className="text-4xl md:text-6xl font-bold text-white mb-4"
+          className="text-4xl md:text-6xl font-bold text-orange-500 mb-4"
         >
           {displayText}
           <span className="blinking-cursor">{cursor}</span>
