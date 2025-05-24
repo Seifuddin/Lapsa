68698decb3ca2ad0afabc0e6047a955ef69919a4
unknown <nguruedwin46@gmail.com>
2025-05-24 23:33:04 +0300

    reversed changes

--- components/Why.js
+++ components/Why.js
@@ -35,7 +35,7 @@ export default function Why() {
   ];
 
   return (
-    <section className="relative bg-gray-50 z-0 py-12 mx-auto">
+    <section className="relative bg-gray-200 z-0 py-12 mx-auto">
       {/* Background image */}
       <div className="absolute inset-0 -z-10 opacity-30">
         <img
